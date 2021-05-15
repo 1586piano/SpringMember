7915abc1d1d22ed29cbb20b366ce0d62f5047e19
박송희(Park Songhee)(parksonghee) <parksonghee@ncsoft.com>
2021-05-15 17:43:43 +0900

    initial commit

---
+++ src/main/java/com/example/memberweb/MemberWebApplication.java
@@ -0,0 +1,14 @@
+package com.example.memberweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberWebApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/memberweb/MemberWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memberweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
